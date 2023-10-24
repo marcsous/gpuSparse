00f85499a54d0d7c08765ff6243c9b79d7478356
Mark Bydder <17042435+marcsous@users.noreply.github.com>
2023-10-24 13:32:22 -0700

    Add files via upload

--- test_gpuSparse.m
+++ test_gpuSparse.m
@@ -2,23 +2,35 @@
 clear all
 reset(gpuDevice(1))
 
-M = 221401;
-N = 213331;
+M = 121401;
+N = 113331;
 P = 5e-4;
 
+%M = 4;
+%N = 4;
+%P = 1;
+
 disp('---SETUP---')
+
+rand('state',0);
+randn('state',0);
 
 tic; fprintf('Making sparse... ')
 A = sprandn(M,N,P);
 toc
+
+% remove unwanted precision
+[i j v] = find(A);
+v = double(single(v));
+A = sparse(i,j,v,M,N);
 
 tic; fprintf('Converting to gpuSparse... ')
 a = gpuSparse(A); validate(a)
 toc
 
 fprintf('Sorted index conversion to gpuSparse: ')
-[i j v] = find(A);
-tic; 
+[i j v] = find(a);
+tic;
 b = gpuSparse(i,j,v,M,N); validate(b)
 fprintf('errors = [%i %i %i]. ',any(a.row~=b.row),any(a.col~=b.col),any(a.val~=b.val))
 toc
@@ -36,6 +48,9 @@ toc
 x = randn(N,1,'gpuArray');
 y = randn(M,1,'gpuArray');
 
+% remove unwanted precision
+x = double(single(x));
+y = double(single(y));
 
 %% Expected failues (bounds etc)
 disp('---CATCH ERRORS---')
@@ -89,6 +104,12 @@ disp(['  Ax            ' num2str(norm(A*x-a*single(x),Inf))])
 disp(['  A''*y          ' num2str(norm(A'*y-a'*single(y),Inf))])
 
 B = sprandn(M,N,P);
+
+% remove unwanted precision
+[i j v] = find(B);
+v = double(single(v));
+B = sparse(i,j,v,M,N);
+
 b = gpuSparse(B); validate(b)
 
 C=A+B;
@@ -109,14 +130,14 @@ d = a - full_transpose(full_transpose(a)); validate(d)
 disp(['max(a-a'''')      ' num2str(max(d.val)) '  (full_transpose)'])
 disp(['min(a-a'''')      ' num2str(min(d.val)) '  (full_transpose)'])
 
-B = randn(N,3);
+B = double(single(randn(N,3)));
 b = gpuArray(B);
 
 C = A*B;
 c = a*single(b);
 disp(['(A*B-a*b)       ' num2str([norm([C-c],Inf)])])
 
-B = randn(M,4);
+B = double(single(randn(M,4)));
 b = gpuArray(B);
 
 C = A'*B;
@@ -130,6 +151,12 @@ disp('---MISCELLANEOUS---')
 % mixed real/complex multiplies
 
 A = A + 1i*sprandn(A);
+
+% remove unwanted precision
+[i j v] = find(A);
+v = double(single(v));
+A = sparse(i,j,v,M,N);
+
 a = gpuSparse(A); validate(a)
 
 x = single(randn(N,1) + 1i*randn(N,1,'gpuArray'));
@@ -197,12 +224,17 @@ fprintf('   %i %i %g\n',norm(i-i2),norm(j-j2),norm(single(v)-v2))
 fprintf('   %i %i %g\n',norm(i-i2),norm(j-j2),norm(single(v)-v2))
 
 disp('nonzeros')
-disp(norm(nonzeros(A)-nonzeros(a),inf))
+disp(norm(nonzeros(A)-nonzeros(a),inf)) % expected failure - different order
 disp(norm(nonzeros(A')-nonzeros(a'),inf))
 disp(norm(nonzeros(A.')-nonzeros(a.'),inf))
 
 disp('addition')
 B = sprandn(M,N,P);
+
+[i j v] = find(B); % remove unwanted precision
+v = double(single(v));
+B = sparse(i,j,v,M,N);
+
 b = gpuSparse(B); validate(b)
 
 A = real(A); B = real(B);
@@ -215,6 +247,13 @@ disp(norm((A'+B') - sparse(a'+b'),Inf))
 disp(norm((A.'+B.') - sparse(a.'+b.'),Inf))
 disp(norm((A+B)' - sparse((a+b)'),Inf))
 
+disp('cat')
+C = [A;B];
+c = [a;b];
+disp(norm(C-c,'fro'))
+C = [A B];
+c = [a b];
+disp(norm(C-c,'fro'))
 
 %% timings
 disp('---TIMINGS---')
@@ -229,10 +268,12 @@ for j = 1:2
     y = double(y);
     
     % to test mm as well as mv multiply
-    if j==2
+    if j==1
+        fprintf('\n============= Matrix-vector multiply =============\n');
+    else
         x = repmat(x,1,2);
         y = repmat(y,1,2);
-        fprintf('\n== Matrix multiply (cols %i) ==\n',size(x,2));
+        fprintf('\n========= Matrix-matrix multiply (cols %i) =========\n',size(x,2));
     end
     
     tic; fprintf('A*x  (sparse)   : ')
@@ -280,20 +321,20 @@ for j = 1:2
     x = single(x);
     y = single(y);
     
-    tic; fprintf('\na*x  (gpuSparse): ')
+    tic; fprintf('\nA*x  (gpuSparse): ')
     for k = 1:20
         z = a*x; wait(gpuDevice);
     end
     toc;
     
     at = full_transpose(a); validate(at)
-    tic; fprintf('at*y (gpuSparse): ')
+    tic; fprintf('At*y (gpuSparse): ')
     for k = 1:20
         z = at*y; wait(gpuDevice);
     end
     toc;
     
-    tic; fprintf('a''*y (gpuSparse): ')
+    tic; fprintf('A''*y (gpuSparse): ')
     for k = 1:20
         z = a'*y; wait(gpuDevice);
     end
